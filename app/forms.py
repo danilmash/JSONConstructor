31208bf5f7434fbad43ba29ca0bf5f2c0b00fcd9
danilmash <danil.mashentsev@mail.ru>
2024-03-12 14:54:37 +0300

    Ввод названия файла, другие названия для полей формы

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,21 @@
+# -*- coding: utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, SelectField, TextAreaField
 from wtforms import validators
 
 class JSONForm(FlaskForm):
     bot_type = SelectField('Тип бота', choices=[('chat_gpt', 'ChatGPT'), ('yandex_gpt', 'YandexGPT')], validators=[validators.DataRequired()])
-    language = SelectField('Изучаемый язык', choices=[('eng', 'English'), ('ge', 'German')], validators=[validators.DataRequired()])
-    lesson_type = SelectField('Тип уроки', choices=[('topic_speak','Разговор на выбранную тему'), ('words_check', 'Проверка слов на выбранную тему')], validators=[validators.DataRequired()])
-    topic = TextAreaField('Тема урока')
-    text = TextAreaField('Текст для ответов на вопросы')
-    save_to_file = BooleanField('Сохранять урок в файл', default=True)
+    
+    role_or_task = SelectField('Изучаемый язык', 
+                            choices=[('Представь что ты преподаватель английского языка, а я твой ученик', 'English'), 
+                                    ('Представь что ты преподаватель немецкого языка, а я твой ученик', 'German')], 
+                            validators=[validators.DataRequired()]) 
+    
+    lesson_type = SelectField('Тип уроки', choices=[('Диалог на определённую тему','Диалог на определённую тему'), 
+                                                    ('Проверка слов на определённую тему', 'Проверка слов на определённую тему')], 
+                                        validators=[validators.DataRequired()])
+    
+    topic_of_dialogue = TextAreaField('Тема урока')
+    text_for_questions = TextAreaField('Текст для ответов на вопросы')
+    save_lesson_to_file = BooleanField('Сохранять урок в файл', default=True)
 
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import render_template, request, jsonify, send_file
 from app import app
 from app.forms import JSONForm
@@ -5,26 +6,29 @@ import json
 import tempfile
 import os
 
+
 @app.route('/', methods=['GET', 'POST'])
 def my_form():
     form = JSONForm()
     if request.method == 'POST':
         data = {
             'bot_type': request.form.get('bot_type'),
-            'language': request.form.get('language'),
+            'role_or_task': request.form.get('role_or_task'),
             'lesson_type': request.form.get('lesson_type'),
-            'topic': request.form.get('topic'),
-            'text': request.form.get('text'),
-            'save_to_file': request.form.get('save_to_file'),
+            'topic_of_dialogue': request.form.get('topic_of_dialogue'),
+            'text_for_questions': request.form.get('text_for_questions'),
+            'save_lesson_to_file': request.form.get('save_lesson_to_file'),
         }
 
-        with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
-            json.dump(data, temp_file)
+        with tempfile.NamedTemporaryFile(mode='w+', delete=False, encoding='utf-8') as temp_file:
+            json.dump(data, temp_file, ensure_ascii=False)
             temp_file_path = temp_file.name
 
-        return send_file(temp_file_path, as_attachment=True, download_name='form_data.json')
+        if request.form.get('filename'):
+            return send_file(temp_file_path, as_attachment=True, download_name='%s.json' % (request.form.get('filename')))
+        else:
+            return send_file(temp_file_path, as_attachment=True, download_name='form-data.json')
 
-        os.remove(temp_file_path)
 
 
     return render_template('index.html', form=form)
